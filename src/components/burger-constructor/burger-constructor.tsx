30e32ba4fd62e0fbbae6b5fd43a5d7307fefa2b3
K_Makolov <koss4ok@gmail.com>
2026-09-15 15:20:00 +0500

    Настроена базовая разметка для конструктора

--- src/components/burger-constructor/burger-constructor.tsx
+++ src/components/burger-constructor/burger-constructor.tsx
@@ -1,3 +1,10 @@
+import {
+  Button,
+  ConstructorElement,
+  CurrencyIcon,
+  DragIcon,
+} from '@krgaa/react-developer-burger-ui-components';
+
 import type { TIngredient } from '@utils/types';
 
 import styles from './burger-constructor.module.css';
@@ -9,7 +16,60 @@ type TBurgerConstructorProps = {
 export const BurgerConstructor = ({
   ingredients,
 }: TBurgerConstructorProps): React.JSX.Element => {
-  console.log(ingredients);
+  const bun = ingredients.find((ingredient) => ingredient.type === 'bun');
+  const fillings = ingredients.filter((ingredient) => ingredient.type !== 'bun');
+  const totalPrice =
+    (bun ? bun.price * 2 : 0) +
+    fillings.reduce((total, ingredient) => total + ingredient.price, 0);
 
-  return <section className={styles.burger_constructor}></section>;
+  return (
+    <section className={styles.burger_constructor}>
+      <div className={styles.bun}>
+        {bun && (
+          <ConstructorElement
+            type="top"
+            isLocked
+            text={`${bun.name} (верх)`}
+            price={bun.price}
+            thumbnail={bun.image}
+            extraClass={styles.element}
+          />
+        )}
+      </div>
+      <div className={`${styles.fillings} custom-scroll`}>
+        {fillings.map((ingredient) => (
+          <div key={ingredient._id} className={styles.filling}>
+            <DragIcon type="primary" />
+            <ConstructorElement
+              text={ingredient.name}
+              price={ingredient.price}
+              thumbnail={ingredient.image}
+              extraClass={styles.element}
+            />
+          </div>
+        ))}
+      </div>
+      <div className={styles.bun}>
+        {bun && (
+          <ConstructorElement
+            type="bottom"
+            isLocked
+            text={`${bun.name} (низ)`}
+            price={bun.price}
+            thumbnail={bun.image}
+            extraClass={styles.element}
+          />
+        )}
+      </div>
+      <div className={styles.order}>
+        <div className={styles.total}>
+          <span className="text text_type_digits-medium">{totalPrice}</span>
+          <CurrencyIcon type="primary" />
+        </div>
+        <Button htmlType="button" type="primary" size="large">
+          Оформить заказ
+        </Button>
+      </div>
+    </section>
+  );
 };
